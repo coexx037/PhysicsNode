3ce6ceb28e045c6c11796f603658eb836dab548b
David Coe <coexx037@gmail.com>
2017-06-24 20:22:24 +0000

    test again

--- db.js
+++ db.js
@@ -4,9 +4,10 @@ var connection = mysql.createConnection({
         host: 'us-cdbr-iron-east-03.cleardb.net',
         user: 'b73e052c103438',
         password: 'f18555ce',
-        database: 'heroku_4f4874e0596111c',
+        database: 'heroku_4f4874e0596111',
         multipleStatements: true
     })
 
 module.exports = connection;
 
+//mysql://b73e052c103438:f18555ce@us-cdbr-iron-east-03.cleardb.net/heroku_4f4874e0596111c?reconnect=true
