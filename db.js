6aaedfbc43bf682b7159924d2799d38b4495ed1a
David Coe <coexx037@gmail.com>
2017-06-24 19:57:23 +0000

    update db.js

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql')
 
 var connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'Physics',
+        host: 'us-cdbr-iron-east-03.cleardb.net',
+        user: 'b73e052c103438',
+        password: 'f18555ce',
+        database: 'heroku_4f4874e0596111c',
         multipleStatements: true
     })
 
 module.exports = connection;
+
