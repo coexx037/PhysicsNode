80cac31ad7d2982816c735852af163e0b95e3b3f
David Coe <coexx037@gmail.com>
2017-07-04 22:13:58 +0000

    fixed bcrypt

--- passport.js
+++ passport.js
@@ -3,32 +3,41 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const express = require('express')
 var app = express()
-var bcrypt = require('bcrypt-nodejs')
+var bcrypt = require('bcrypt')
 var connection = require('./db')
 
 
-var sql = 'select * from User_Dfn where email = ?'
-var sql2 = 'insert into User_Dfn (first_name, last_name, email, password) values (?,?,?,?)' 
+var sql = 'select * from user_dfn where email = ?'
+var sql2 = 'insert into user_dfn (first_name, last_name, email, password) values (?,?,?,?)' 
 
 function authenticate(email, password, done){
     connection.query(sql, [email], function(err, rows){
+        console.log(rows[0].password);
+        console.log(password);
         
         if(err){
             console.log('system error!')
             return done(err);
         }
-        if(!rows || !bcrypt.compareSync(password, rows[0].password)){
-            console.log('cant find in db!')
-            return done(null, false, {message: 'Incorrect password or username'})
+        if(!rows.length){
+            console.log('cant find email in db!')
+            return done(null, false, {message: 'Incorrect email, cant find the email in db'})
         }
+        if(!bcrypt.compareSync(password, rows[0].password)){
+                console.log('bcrypt kinda worked but paswword doesnt match')
+                return done(null, false, {message: 'incorrect password'})
+            }
         return done(null, rows[0])
-    })
-}
+            
+        })
+    }
+        
 
 function register(req, email, password, done){
     connection.query(sql, [email], function(err, rows){
         
         if(err){
+            console.log('ooooooooppppppssssss');
             return done(err)
         }
         if(rows[0]){
@@ -42,8 +51,10 @@ function register(req, email, password, done){
             first_name: req.body.first_name,
             last_name: req.body.last_name,
             email: email,
-            password: bcrypt.hashSync(password),
+            password: bcrypt.hashSync(password, 10)
         }
+        
+        console.log(newUser);
         
   
         
