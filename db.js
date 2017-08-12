8dcb67cb2284ec2612d60fefe2ab89cfdf1f854e
David Coe <coexx037@gmail.com>
2017-08-12 17:52:58 +0000

    hide details

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 const mysql = require('mysql')
 
 var pool = mysql.createPool({
-        host: 'us-cdbr-iron-east-03.cleardb.net',
-        user: 'b73e052c103438',
-        password: 'f18555ce',
-        database: 'heroku_4f4874e0596111c',
+        host: 'xxx',
+        user: 'xxx',
+        password: 'xxx',
+        database: 'xxx',
         multipleStatements: true
     })
 
 
 
 module.exports = pool;
-
-//mysql://b73e052c103438:f18555ce@us-cdbr-iron-east-03.cleardb.net/heroku_4f4874e0596111c?reconnect=true
